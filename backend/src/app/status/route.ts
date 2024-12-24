163830520bb6bb9f304767022959f0bfbae1615e
Zdeněk Tomis <zdnek.tomis@gmail.com>
2024-12-24 23:09:13 +0100

    backend fixes

--- backend/src/app/status/route.ts
+++ backend/src/app/status/route.ts
@@ -1,14 +1,20 @@
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
 
+const deploytime = process.env.PAYLOAD_PUBLIC_DEPLOY_TIME;
+const gitref = process.env.PAYLOAD_PUBLIC_VERSION;
+const local = process.env.PAYLOAD_PUBLIC_LOCAL;
+
 export const GET = async () => {
   const payload = await getPayload({
     config: configPromise,
   })
 
-
   return Response.json({
     status: 'ok',
+    deploytime,
+    gitref,
+    local,
     mode: process.env.NODE_ENV,
   })
 }
